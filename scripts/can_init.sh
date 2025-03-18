d1353b5d47f5e51a85da821ce8f7220c29689974
Dylan Miller <djm2@ualberta.ca>
2025-03-18 12:42:53 -0600

    CMakeLists fix and include script to setup can interface

---
+++ scripts/can_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo modprobe peak_pci
+
+sudo ip link set can0 type can bitrate 1000000
+sudo ip link set up can0
+
+sudo ip link set can1 type can bitrate 1000000
+sudo ip link set up can1
