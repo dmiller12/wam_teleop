80781ebcfb52eea75182f55c7f54e952e663e25a
Dylan Miller <djm2@ualberta.ca>
2025-03-28 11:58:35 -0600

    WIP: adding follower pos-vel ref

--- src/follower.h
+++ src/follower.h
@@ -23,11 +23,16 @@ class Follower : public barrett::systems::System {
                       int send_port = 5555, const std::string& sysName = "Follower")
         : System(sysName)
         , theirJp(0.0)
+        , theirJv(0.0)
+        , control(0.0)
         , wamJPIn(this)
         , wamJVIn(this)
         , wamJPOutput(this, &jpOutputValue)
         , udp_handler(remoteHost, send_port, rec_port)
         , state(State::INIT) {
+
+        kp << 600, 700, 250, 120;
+        kd << 30, 25, 15, 10;
 
         if (em != NULL) {
             em->startManaging(*this);
@@ -51,7 +56,7 @@ class Follower : public barrett::systems::System {
     }
 
   protected:
-    typename Output<jp_type>::Value* jpOutputValue;
+    typename Output<jt_type>::Value* jtOutputValue;
     jp_type wamJP;
     jv_type wamJV;
     Eigen::Matrix<double, DOF, 1> sendJpMsg;
@@ -73,6 +78,7 @@ class Follower : public barrett::systems::System {
         if (received_data && (now - received_data->timestamp <= TIMEOUT_DURATION)) {
 
             theirJp = received_data->jp;
+            theirJv = received_data->jv;
         } else {
             if (state == State::LINKED) {
                 std::cout << "lost link" << std::endl;
@@ -82,20 +88,25 @@ class Follower : public barrett::systems::System {
 
         switch (state) {
             case State::INIT:
-                jpOutputValue->setData(&wamJP);
+                control.setZero();
+                jtOutputValue->setData(&control);
                 break;
             case State::LINKED:
                 // Active teleop. Only the callee can transition to LINKED
-                jpOutputValue->setData(&theirJp);
+                control = compute_control(theirJp, theirJv, wamJP, wamJV);
+                jtOutputValue->setData(&control);
                 break;
             case State::UNLINKED:
                 // Changed to unlinked with either timeout or callee.
-                jpOutputValue->setData(&wamJP);
+                control.setZero();
+                jtOutputValue->setData(&control);
                 break;
         }
     }
 
     jp_type theirJp;
+    jp_type theirJv;
+    jt_type control;
 
   private:
     DISALLOW_COPY_AND_ASSIGN(Follower);
@@ -104,4 +115,13 @@ class Follower : public barrett::systems::System {
     UDPHandler<DOF> udp_handler;
     const std::chrono::milliseconds TIMEOUT_DURATION = std::chrono::milliseconds(20);
     State state;
+    Eigen::Matrix<double, DOF, 1> kp;
+    Eigen::Matrix<double, DOF, 1> kd;
+
+    jt_type compute_control(const jp_type& ref_pos, const jv_type& ref_vel, const jp_type& cur_pos,
+                            const jv_type& cur_vel) {
+        jt_type pos_term = kp.asDiagonal() * (ref_pos - cur_pos);
+        jt_type vel_term = kd.asDiagonal() * (ref_vel - cur_vel);
+        return pos_term + vel_term;
+    };
 };
