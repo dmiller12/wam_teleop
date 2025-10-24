9b282d98710d593d1bb76dd60090f61c0f706657
Dylan Miller <djm2@ualberta.ca>
2025-10-24 17:21:29 -0600

    modified initial pose

--- src/leader.cpp
+++ src/leader.cpp
@@ -43,9 +43,9 @@ int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam) 
     jp_type SYNC_POS; // the position each WAM should move to before linking
     if (DOF == 4) {
         SYNC_POS[0] = 0.0;
-        SYNC_POS[1] = -1.5;
+        SYNC_POS[1] = -1.57;
         SYNC_POS[2] = 0.0;
-        SYNC_POS[3] = 1.5;
+        SYNC_POS[3] = 1.57;
 
     } else {
         printf("Error: Only 4 DOF wam supported\n");
